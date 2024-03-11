41ca2b428e9b49caf914623869db45fd98c791ab
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-03-11 15:03:16 +0100

    changed some url

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'https://att-signin.vercel.app',
-    // 'https://att-auth.vercel.app',
-    // 'https://att-welcome.vercel.app',
     'https://one-note-welcome.vercel.app',
-    'https://at-welcome-auth.vercel.app',
+    'https://welcome-at.vercel.app',
   ],
   credentials: true,
 }
@@ -29,7 +27,7 @@ app.use(express.json())
 
 app.post('/add-data', addData)
 app.get('/get-data', getData)
-app.post('/login', send)
+app.post('/main', send)
 app.post('/one-note-auth', Auth)
 
 app.listen(3000, () => console.log('listening at 3000'))
